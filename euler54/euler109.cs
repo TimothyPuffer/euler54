0682de049352ff8cc825a16131b49752653a5c34
Timothy Puffer Work <timothypuffer@gmail.com>
2014-01-16 10:20:36 -0600

    109

---
+++ euler54/euler109.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler54
+{
+    class euler109
+    {
+        public static int GetResults()
+        {
+            var values = from h in Enumerable.Range(1, 20)
+                         from m in Enumerable.Range(1, 3)
+                         select new Tuple<int, int>(h * m, m);
+
+            List<Tuple<int, int>> boardHits = new List<Tuple<int, int>>();
+
+            boardHits.Add(new Tuple<int, int>(25, 1));
+            boardHits.Add(new Tuple<int, int>(50, 2));
+            boardHits.AddRange(values);
+
+            var Results = from s in Enumerable.Range(1, 99)
+                          let h1 = DartsUsed1(boardHits, s)
+                          let h2 = DartsUsed2(boardHits, s)
+                          let h3 = DartsUsed3(boardHits, s)
+                          select h1 + h2 + h3;
+
+            return Results.Sum();
+        }
+
+        static int DartsUsed1(List<Tuple<int, int>> boardHits, int score)
+        {
+            return (from h in boardHits
+                    where h.Item2 == 2 && h.Item1 == score
+                    select h).Distinct().Count();
+        }
+
+        static int DartsUsed2(List<Tuple<int, int>> boardHits, int score)
+        {
+            return (from h1 in boardHits
+                    from h2 in boardHits
+                    where h2.Item2 == 2 && h1.Item1 + h2.Item1 == score
+                    select new Tuple<Tuple<int, int>, Tuple<int, int>>(h1, h2)).Distinct().Count();
+        }
+
+        static int DartsUsed3(List<Tuple<int, int>> boardHits, int score)
+        {
+            var Suc = from h1 in boardHits
+                      from h2 in boardHits
+                      from h3 in boardHits
+                      where h3.Item2 == 2 && h1.Item1 + h2.Item1 + h3.Item1 == score
+                      select new Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>(h1, h2, h3);
+
+            List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>> test = new List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>>();
+
+            foreach (var t in Suc)
+            {
+                if (!test.Contains(t) && !test.Contains(new Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>(t.Item2, t.Item1, t.Item3)))
+                {
+                    test.Add(t);
+                }
+            }
+            return test.Count;
+        }
+
+
+
+    }
+}
---
+++ euler54/euler54.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler54
+{
+    static class euler54
+    {
+
+        public static int GetResults()
+        {
+            string[] allLines = CardText.String.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var playerHands = from t in allLines
+                              let split10 = t.Split(" ".ToArray())
+                              let card10 = split10.Select(s => conCard(s))
+                              select new { Player1 = card10.Take(5).ToArray(), Player2 = card10.Skip(5).ToArray() };
+
+            var player1Win = from p in playerHands
+                             let p1 = GetHandValue(p.Player1)
+                             let p2 = GetHandValue(p.Player2)
+                             where FirstIsGreater(p1, p2)
+                             select 1;
+
+            return player1Win.Count();
+        }
+
+        static int[] GetHandValue(Tuple<int, string>[] hand)
+        {
+
+
+            Predicate<Tuple<int, string>[]> isFlush = c => c.Select(f => f.Item2).Distinct().Count() == 1;
+            List<int> handRanks = hand.Select(c => c.Item1).OrderByDescending(c => c).ToList();
+            int straightValue = IsStraight(handRanks);
+
+
+
+            if (isFlush(hand) && straightValue != -1)
+                return new int[] { 10, straightValue };
+
+            List<int> kind4 = HasSets(handRanks, new int[] { 4, 1 });
+            if (kind4 != null)
+            {
+                kind4.Insert(0, 9);
+                return kind4.ToArray();
+            }
+
+            List<int> kind32 = HasSets(handRanks, new int[] { 3, 2 });
+            if (kind32 != null)
+            {
+                kind32.Insert(0, 8);
+                return kind32.ToArray();
+            }
+
+            if (isFlush(hand))
+                return new int[] { 7, handRanks.Max() };
+
+            if (straightValue != -1)
+                return new int[] { 6, straightValue };
+
+            List<int> kind311 = HasSets(handRanks, new int[] { 3, 1, 1 });
+            if (kind311 != null)
+            {
+                kind311.Insert(0, 5);
+                return kind311.ToArray();
+            }
+
+            List<int> kind221 = HasSets(handRanks, new int[] { 2, 2, 1 });
+            if (kind221 != null)
+            {
+                kind221.Insert(0, 4);
+                return kind221.ToArray();
+            }
+
+            List<int> kind2111 = HasSets(handRanks, new int[] { 2, 1, 1, 1 });
+            if (kind2111 != null)
+            {
+                kind2111.Insert(0, 3);
+                return kind2111.ToArray();
+            }
+
+            handRanks.Insert(0, 0);
+            return handRanks.ToArray();
+
+        }
+
+        static List<int> HasSets(List<int> values, int[] findSet)
+        {
+            var setGroupings = from t in values
+                               group t by t into g
+                               orderby g.Count() descending, g.Key descending
+                               select new { CardRank = g.Key, SetCount = g.Count() };
+
+            var set = from s in setGroupings
+                      orderby s.SetCount descending
+                      select s.SetCount;
+
+            if (Enumerable.SequenceEqual(set, findSet))
+            {
+                return setGroupings.Select(c => c.CardRank).ToList();
+            }
+
+            return null;
+        }
+
+        static int IsStraight(List<int> values)
+        {
+            int[] lowAce = { 14, 2, 3, 4, 5 };
+
+            if (values.Distinct().Count() != 5)
+                return -1;
+
+            if (values.Intersect(lowAce).Count() == 5)
+                return 5;
+
+            if (values.Max() - values.Min() == 4)
+                return values.Max();
+
+            return -1;
+
+        }
+
+        static bool FirstIsGreater(int[] player1Value, int[] playerValue)
+        {
+            int i = 0;
+            while (true)
+            {
+                if (player1Value[i] > playerValue[i])
+                    return true;
+
+                if (player1Value[i] < playerValue[i])
+                    return false;
+
+                i = i + 1;
+            }
+
+        }
+
+        static Tuple<int, string> conCard(string s)
+        {
+            return new Tuple<int, string>(CardValue(s.Substring(0, 1)), s.Substring(1));
+
+        }
+
+        static int CardValue(string s)
+        {
+            switch (s)
+            {
+                case "A":
+                    return 14;
+                case "K":
+                    return 13;
+                case "Q":
+                    return 12;
+                case "J":
+                    return 11;
+                case "T":
+                    return 10;
+                default:
+                    return int.Parse(s);
+            }
+        }
+
+    }
+}
